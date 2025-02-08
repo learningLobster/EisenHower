98d852d6cee9fb8e7f5d5b4a9a6112b2b7fcd3b7
learningLobster <118805014+learningLobster@users.noreply.github.com>
2025-02-09 00:23:42 +0100

    Version 1.2(Display)

---
+++ config.py
@@ -0,0 +1,11 @@
+def height_prct(screen_height, percentage):
+    """
+        Returns a percentage.
+    """
+    return (screen_height / 100) * percentage
+
+def width_prct(screen_width, percentage):
+    """
+        Returns a percentage.
+    """
+    return (screen_width / 100) * percentage
--- main.py
+++ main.py
@@ -1,63 +1,48 @@
-from tabulate import tabulate
-
-# Table left column
-IMPORTANT, URGENT, NOT_URGENT, NOT_IMPORTANT = ["important"], ["urgent"], ["not urgent"], ["not important"]
-matrix = []
-
-# first_sub_list = important, second_sub_list = urgent, third_sub_list = not urgent, fourth_sub_list = not important
-def add_element(main_list, first_sub_list, second_sub_list, third_sub_list, fourth_sub_list):
-    """
-    This method creates a EisenHower matrix.
-
-    Parameters:
-        main_list (list): The main list containing all elements.
-        first_sub_list (list): table left column, first row(important).
-        second_sub_list (list): table left column, second row(urgent).
-        third_sub_list (list): table left column, third row(not urgent).
-        fourth_sub_list (list): table left column, fourth row(not important).
-
-    Returns:
-        None.
-
-    Action:
-        - Checks if 'not' is present in the element.
-        - Decides which sublist to place the element into.
-
-    Example:
-        >>> main = []
-        >>> first, second, third, fourth = ["important"], ["urgent"], ["not urgent"], ["not important""]
-        >>> add_element(main, first, second, third, fourth)
-    """
-
-    new_element = input("Enter the element: \n-> ").lower()
-    importance = input("Enter the importance: 'important' or 'not important': \n-> ").lower()
-    urgency = input("Enter the urgency: 'urgent' or 'not urgent': \n-> ").lower()
-
-    if 'not' in importance and 'not' in urgency:
-        fourth_sub_list.append(new_element)
-        third_sub_list.append(new_element)
-
-    elif 'not' in importance and 'not' not in urgency:
-        fourth_sub_list.append(new_element)
-        second_sub_list.append(new_element)
-
-    elif 'not' not in importance and 'not' in urgency:
-        first_sub_list.append(new_element)
-        third_sub_list.append(new_element)
-
-    else:
-        first_sub_list.append(new_element)
-        second_sub_list.append(new_element)
-
-    main_list.append(IMPORTANT)
-    main_list.append(URGENT)
-    main_list.append(NOT_URGENT)
-    main_list.append(NOT_IMPORTANT)
-
-def display_matrix(element):
-    # table = [['col 1', 'col 2', 'col 3', 'col 4'], [1, 2222, 30, 500], [4, 55, 6777, 1]]
-    print(tabulate(element, tablefmt="grid"))
+import sys
+import pygame
+import utils
+import config
+from config import width_prct
 
 
-add_element(matrix, IMPORTANT, URGENT, NOT_URGENT, NOT_IMPORTANT)
-display_matrix(matrix)
+class Window:
+    def __init__(self):
+        pygame.init()
+
+        self.width = utils.WIDTH
+        self.height = utils.HEIGHT
+
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
+        self.top_left = pygame.Surface([config.width_prct(self.width, 50), config.height_prct(self.height, 50)])
+        self.top_right = pygame.Surface([config.width_prct(self.width, 50), config.height_prct(self.height, 50)])
+        self.bottom_left = pygame.Surface([config.width_prct(self.width, 50), config.height_prct(self.height, 50)])
+        self.bottom_right = pygame.Surface([config.width_prct(self.width, 50), config.height_prct(self.height, 50)])
+
+        self.top_left.fill('red')
+        self.top_right.fill('blue')
+        self.bottom_left.fill('green')
+        self.bottom_right.fill('yellow')
+
+        self.clock = pygame.time.Clock()
+
+    def event_handler(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def mainloop(self):
+        while True:
+            self.event_handler()
+            self.screen.blit(self.top_left, (0,0))
+            self.screen.blit(self.top_right, (0, config.height_prct(self.height, 50)))
+            self.screen.blit(self.bottom_left, (config.width_prct(self.width, 50), config.height_prct(self.height, 50)))
+            self.screen.blit(self.bottom_right, (config.width_prct(self.width, 50), 0))
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    window = Window()
+    window.mainloop()
